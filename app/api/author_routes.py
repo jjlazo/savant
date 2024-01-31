8f54ed42b6c6ce789064a15a1649e1c5cca31e35
juniper <jjlazo96@gmail.com>
2024-01-31 13:54:00 -0500

    mise en place

---
+++ app/api/author_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request
+from app.models import Author, db
+from app.forms import AuthorForm
+from flask_login import current_user, login_user, logout_user, login_required
+
+author_routes = Blueprint('author', __name__)
+
+
+@author_routes.route('/')
+def get_all_authors():
+    authors = Author.query.all()
+    if authors:
+        return {"Authors": [author.to_dict() for author in authors]}
+    return {'errors': {'message': 'Authors Not Found'}}, 404
