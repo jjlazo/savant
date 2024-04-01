5ae3dd02f39a9fbd84201188dcd90b2acbf0a39b
juniper <jjlazo96@gmail.com>
2024-04-01 16:16:37 -0500

    annotations

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 
+from app.seeds.annotations import seed_annotations, undo_annotations
 from app.seeds.poems import seed_poems, undo_poems
 from .users import seed_users, undo_users
 from .authors import seed_authors, undo_authors
@@ -20,6 +21,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_annotations()
         undo_comments()
         undo_poems()
         undo_authors()
@@ -28,14 +30,14 @@ def seed():
     seed_authors()
     seed_poems()
     seed_comments()
-    # seed_annotations()
+    seed_annotations()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    # undo_annotations()
+    undo_annotations()
     undo_comments()
     undo_poems()
     undo_authors()
